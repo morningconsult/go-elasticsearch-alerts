30cb93707786c0a8ef6bb8b5ad04c210bb930534
Dilan Bellinghoven <ddbellinghoven@gmail.com>
2018-11-28 13:41:55 -0500

    ci/tasks/build-release.yml: Implemented goreleaser

--- ci/build-release.sh
+++ ci/build-release.sh
@@ -11,3 +11,33 @@
 # on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
 # express or implied. See the License for the specific language governing
 # permissions and limitations under the License.
+
+set -e
+
+readonly PROJECT="github.com/morningconsult/go-elasticsearch-alerts"
+readonly GORELEASER_VERSION=v0.88.0
+
+echo "==> Installing APK dependencies"
+
+apk add -qU --no-progress \
+  make \
+  bash \
+  git \
+  gnupg
+
+echo "==> Installing goreleaser $GORELEASER_VERSION"
+
+wget --quiet -O /tmp/goreleaser.tar.gz https://github.com/goreleaser/goreleaser/releases/download/${GORELEASER_VERSION}/goreleaser_Linux_x86_64.tar.gz
+tar xzf /tmp/goreleaser.tar.gz -C /tmp
+mv /tmp/goreleaser /usr/local/bin
+
+mkdir -p "${GOPATH}/src/${PROJECT}"
+cp -r . "${GOPATH}/src/${PROJECT}"
+cd "${GOPATH}/src/${PROJECT}"
+
+echo "==> Running unit tests"
+
+CGO_ENABLED=0 make test
+
+goreleaser release \
+  --rm-dist
