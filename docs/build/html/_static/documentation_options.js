6a0cee414024d11e7108b7b2d9e407d3f2c94918
Dilan Bellinghoven <ddbellinghoven@gmail.com>
2018-12-11 13:13:15 -0500

    docs: Version bump

--- docs/build/html/_static/documentation_options.js
+++ docs/build/html/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'v0.0.21',
+    VERSION: 'v0.0.22',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
